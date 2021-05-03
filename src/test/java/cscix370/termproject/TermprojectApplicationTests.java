82ab08d80f9c3d2d36eee14d522600e784b5fd8d
Cameron Hubbard <camhubbard@hotmail.com>
2021-05-03 15:54:49 -0400

    initial setup

---
+++ src/test/java/cscix370/termproject/TermprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package cscix370.termproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
