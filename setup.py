78d6f24e38944e200e1cf0d5193a0a928cb9e1c4
Cameron Hubbard <camhubbard@hotmail.com>
2021-05-04 19:15:49 -0400

    Finished database script/removed tuples

--- setup.py
+++ setup.py
@@ -1,81 +1,67 @@
 import mysql.connector
-from mysql.connector import Error
+# from mysql.connector import Error
 import pandas as pd
 import pyodbc
 import os
 import time
 
-# Arpinar's code. Feel free to add or remove as needed.
-username = input('What is your mysql username? ')
-password = input('What is your mysql password? ')
-input_file = 'music.sql'
-mysql_login = 'mysql -u' + username + ' -p' + password + ' < ' + input_file
-os.system(mysql_login)
-os.system('cd MusicApp')
-os.system('javac -cp MusicApp/lib/mysql-connector-java-8.0.16.jar  -d MusicApp/bin MusicApp/src/controller/*.java MusicApp/src/model/*.java MusicApp/src/view/*.java MusicApp/src/model/DAO/*.java')
-time.sleep(20)
-os.system('java MusicApp/bin/controller/MusicApp')
+# # Arpinar's code. Feel free to add or remove as needed.
+# username = input('What is your mysql username? ')
+# password = input('What is your mysql password? ')
+# input_file = 'music.sql'
+# mysql_login = 'mysql -u' + username + ' -p' + password + ' < ' + input_file
+# os.system(mysql_login)
+# os.system('cd MusicApp')
+# os.system('javac -cp MusicApp/lib/mysql-connector-java-8.0.16.jar  -d MusicApp/bin MusicApp/src/controller/*.java MusicApp/src/model/*.java MusicApp/src/view/*.java MusicApp/src/model/DAO/*.java')
+# time.sleep(20)
+# os.system('java MusicApp/bin/controller/MusicApp')
 
 # Connect to SQL server. ENTER YOUR SERVER NAME WHERE IT SAYS 'Server=SERVERNAME;'
-conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=SERVERNAME;'
-                      'Database=TermDB;'
-                      'Trusted_Connection=yes;')
-cursor = conn.cursor()
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  db="term_project"
+)
+
+print(mydb)
+
+cursor = mydb.cursor()
 
 
 # Set up CSV files for SQL
 ratingsRaw = pd.read_csv (r'./ratings.csv')
-ratings = pd.DataFrame(data, columns= ['user_id', 'book_id', 'rating'])
+ratings = pd.DataFrame(ratingsRaw, columns= ['user_id', 'book_id', 'rating'])
 
-to_readRaw = pd.read_csv (r'./to_read')
-to_read = pd.DataFrame(data, columns= ['user_id', 'book_id'])
+to_readRaw = pd.read_csv (r'./to_read.csv')
+to_read = pd.DataFrame(to_readRaw, columns= ['user_id', 'book_id'])
 
 booksRaw = pd.read_csv (r'./books.csv')
-books = pd.DataFrame(data, columns= ['goodreads_book_id', 'book_id', 'best_book_id','work_id', 'books_count', 'isbn', 'authors', 'original_publication_year', 'original_title','title', 'language_code', 'average_rating', 'ratings_count', 'work_ratings_count', 'work_text_reviews_count', 'ratings_1', 'ratings_2', 'ratings_3', 'ratings_4', 'ratings_5'])
+booksRaw = booksRaw.where((pd.notnull(booksRaw)), None)
+books = pd.DataFrame(booksRaw, columns= ['goodreads_book_id', 'book_id', 'best_book_id','work_id', 'books_count', 'isbn', 'authors', 'original_publication_year', 'original_title','title', 'language_code', 'average_rating', 'ratings_count', 'work_ratings_count', 'work_text_reviews_count', 'ratings_1', 'ratings_2', 'ratings_3', 'ratings_4', 'ratings_5'])
 
 book_tagsRaw = pd.read_csv (r'./book_tags.csv')
-book_tags = pd.DataFrame(data, columns= ['goodreads_book_id', 'tag_id', 'count'])
+book_tags = pd.DataFrame(book_tagsRaw, columns= ['goodreads_book_id', 'tag_id', 'count'])
 
 tagsRaw = pd.read_csv (r'./tags.csv')
-tags = pd.DataFrame(data, columns= ['tag_id', 'tag_name'])
+tags = pd.DataFrame(tagsRaw, columns= ['tag_id', 'tag_name'])
 
 
 # Create tables to import CSV data into
-cursor.execute('CREATE TABLE ratings (user_id INT(11), book_id INT(11), rating INT(11))')
-cursor.execute('CREATE TABLE to_read (user_id INT(11), book_id INT(11)')
-cursor.execute('CREATE TABLE books (goodreads_book_id INT(11), book_id INT(11), best_book_id INT(11), work_id INT(11), books_count INT(11), isbn INT(11), authors VARCHAR(100), original_publication_year INT(11), original_title VARCHAR(45), title VARCHAR(45), language_code VARCHAR(45), average_rating FLOAT, ratings_count INT(11), work_ratings_count INT(11), work_text_reviews_count INT(11), ratings_1 INT(11), ratings_2 INT(11), ratings_3 INT(11), ratings_4 INT(11), ratings_5 INT(11))')
-cursor.execute('CREATE TABLE book_tags (goodreads_book_id INT(11), tag_id INT(11), count INT(11))')
-cursor.execute('CREATE TABLE tags (tag_id INT(11), tag_name VARCHAR(45))')
+cursor.execute('CREATE TABLE IF NOT EXISTS ratings (user_id INT(11), book_id INT(11), rating INT(11))')
+cursor.execute('CREATE TABLE IF NOT EXISTS to_read (user_id INT(11), book_id INT(11))')
+cursor.execute('CREATE TABLE IF NOT EXISTS books (goodreads_book_id INT(11), book_id INT(11), best_book_id INT(11), work_id INT(11), books_count INT(11), isbn INT(11), authors VARCHAR(100), original_publication_year INT(11), original_title VARCHAR(45), title VARCHAR(45), language_code VARCHAR(45), average_rating FLOAT, ratings_count INT(11), work_ratings_count INT(11), work_text_reviews_count INT(11), ratings_1 INT(11), ratings_2 INT(11), ratings_3 INT(11), ratings_4 INT(11), ratings_5 INT(11))')
+cursor.execute('CREATE TABLE IF NOT EXISTS book_tags (goodreads_book_id INT(11), tag_id INT(11), count INT(11))')
+cursor.execute('CREATE TABLE IF NOT EXISTS tags (tag_id INT(11), tag_name VARCHAR(45))')
 
 
 # Insert the dataframes into the respective tables
-for row in ratings.itertuples():
-    cursor.execute('''
-                   INSERT INTO TermDB.dbo.ratings (user_id, book_id, rating)
-                   VALUES (?,?,?)
-                   ''',
-                   row.user_id,
-                   row.book_id,
-                   row.rating
-                   )
-
-for row in to_read.itertuples():
-    cursor.execute('''
-                  INSERT INTO TERM.dbo.to_read (goodreads_book_id, tag_id, count)
-                  VALUES (?,?,?)
-                  ''',
-                  row.goodreads_book_id,
-                  row.tag_id,
-                  row.count
-                  )
-
 for row in books.itertuples():
     cursor.execute('''
-                  INSERT INTO TERM.dbo.books (goodreads_book_id, book_id, best_book_id, work_id, books_count, isbn, authors, original_publication_year, original_title,title, language_code, average_rating, ratings_count, work_ratings_count, work_text_reviews_count, ratings_1, ratings_2, ratings_3, ratings_4, ratings_5)
-                  VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+                  INSERT INTO books (goodreads_book_id, book_id, best_book_id, work_id, books_count, isbn, authors, original_publication_year, original_title, title, language_code, average_rating, ratings_count, work_ratings_count, work_text_reviews_count, ratings_1, ratings_2, ratings_3, ratings_4, ratings_5)
+                  VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
                   ''',
-                  row.goodreads_book_id,
+                  (row.goodreads_book_id,
                   row.book_id,
                   row.best_book_id,
                   row.work_id,
@@ -94,26 +80,47 @@ for row in books.itertuples():
                   row.ratings_2,
                   row.ratings_3,
                   row.ratings_4,
-                  row.ratings_5
+                  row.ratings_5)
                   )
 
+mydb.commit()
+
+cc = 1
+for row in ratings.itertuples():
+    sql = "INSERT INTO ratings (user_id, book_id, rating) VALUES (%s,%s,%s)"
+    val = (row.user_id, row.book_id, row.rating)
+    cursor.execute(sql, val)
+    print(cc)
+    cc = cc + 1
+
+mydb.commit()
+for row in to_read.itertuples():
+    cursor.execute('''
+                  INSERT INTO to_read (user_id, book_id)
+                  VALUES (%s,%s)
+                  ''',
+                  (row.user_id, row.book_id)
+                  )
+
+
+mydb.commit()
 for row in book_tags.itertuples():
     cursor.execute('''
-                  INSERT INTO TERM.dbo.book_tags (goodreads_book_id, tag_id, count)
-                  VALUES (?,?,?)
+                  INSERT INTO book_tags (goodreads_book_id, tag_id, count)
+                  VALUES (%s,%s,%s)
                   ''',
-                  row.goodreads_book_id,
+                  (row.goodreads_book_id,
                   row.tag_id,
-                  row.count
+                  row.count)
                   )
-
+mydb.commit()
 for row in tags.itertuples():
     cursor.execute('''
-                  INSERT INTO TERM.dbo.tags (tag_id, tag_name)
-                  VALUES (?,?)
+                  INSERT INTO tags (tag_id, tag_name)
+                  VALUES (%s,%s)
                   ''',
-                  row.tag_id,
-                  row.tag_name
+                  (row.tag_id,
+                  row.tag_name)
                   )
 
-conn.commit()
+mydb.commit()
